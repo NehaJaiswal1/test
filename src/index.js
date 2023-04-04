be457af7747601d3a4d30fe6d6bc8acc16005694
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-04-05 00:20:11 +0530

    add

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+var cors = require('cors');
+const multer = require("multer");
+// const route = require('./route/route.js');
+
+app.use(express.json())
+
+
+app.use(cors())
+
+app.use(multer().any())
+
+
+mongoose.connect("mongodb+srv://nehajaiswal:neha123@nehadb.pcorgpc.mongodb.net/FSOC", {
+    useNewurlParser: true
+})
+    .then(() => console.log("mongoose is connected"))
+    .catch(err => console.log(err))
+
+// app.use('/',route)
+
+app.post('/test-me', function(req, res){
+    console.log('SERVER is running ok');
+    res.send('SERVER is running ok');
+});
+
+app.listen(3000)
+
